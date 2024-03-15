d2fa4532ba74ce5880670f313cb73a43923323b0
Elianora Rose <developer@elianorarose.com>
2024-03-15 14:37:57 -0400

    Adding basic slash command

--- DiscordDemoBot/Program.cs
+++ DiscordDemoBot/Program.cs
@@ -1,7 +1,31 @@
 using DiscordDemoBot;
+using DiscordDemoBot.SlashCommands;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton(serviceProvider => 
+{
+    var discordConfig = new DiscordConfiguration
+    {
+        Token = builder.Configuration["DiscordBot:Token"],
+        TokenType = TokenType.Bot,
+        Intents = DiscordIntents.AllUnprivileged
+    };
+
+    var discordClient = new DiscordClient(discordConfig);
+    var slashCommandsConfig = new SlashCommandsConfiguration
+    {
+        Services = serviceProvider
+    };
+
+    var slashCommands = discordClient.UseSlashCommands(slashCommandsConfig);
+    slashCommands.RegisterCommands<BasicCommands>();
+
+    return discordClient;
+});
 
 var host = builder.Build();
+
 host.Run();
---
+++ DiscordDemoBot/SlashCommands/BasicCommands.cs
@@ -0,0 +1,18 @@
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+
+namespace DiscordDemoBot.SlashCommands;
+
+[SlashCommandGroup("basic", "Perform basic tasks")]
+public class BasicCommands(ILogger<BasicCommands> logger) : ApplicationCommandModule
+{
+    [SlashCommand("ping", "Pings the bot")]
+    public async Task Test(InteractionContext context)  
+    {
+        logger.Log(LogLevel.Debug, "Responding to ping");
+        await context.CreateResponseAsync(InteractionResponseType.Pong, new()
+        {
+            Content = "Pong"
+        });
+    }
+}
