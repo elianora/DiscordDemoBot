c3054b9e0094015c716127877e72167b64eccc92
Elianora Rose <developer@elianorarose.com>
2024-03-22 13:05:19 -0400

    Adding logging line

--- DiscordDemoBot/EventHandlers/BasicHandlers.cs
+++ DiscordDemoBot/EventHandlers/BasicHandlers.cs
@@ -7,8 +7,9 @@ namespace DiscordDemoBot.EventHandlers;
 
 public class BasicHandlers
 {
-    public static async Task OnButtonPressed(DiscordClient _, ComponentInteractionCreateEventArgs args)
+    public static async Task OnButtonPressed(DiscordClient client, ComponentInteractionCreateEventArgs args)
     {
+        client.Logger.Log(LogLevel.Debug, $"Button with ID {args.Id} pressed in channel {args.Channel.Name}...");
         var buttonText = args.Id switch
         {
             ButtonId.Primary => "primary",
