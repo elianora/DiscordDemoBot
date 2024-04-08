1b7ec78dfb82570d7b9ee268d89ab8eb08923bad
Elianora Rose <developer@elianorarose.com>
2024-04-08 14:53:07 -0400

    Changing to use IHostedService interface instead of BackgroundService

--- DiscordDemoBot/Worker.cs
+++ DiscordDemoBot/Worker.cs
@@ -2,21 +2,18 @@ using DSharpPlus;
 
 namespace DiscordDemoBot;
 
-public class Worker(DiscordClient discordClient) : BackgroundService
+public class Worker(DiscordClient discordClient) : IHostedService
 {
-    public override async Task StartAsync(CancellationToken cancellationToken)
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
         discordClient.Logger.Log(LogLevel.Information, "Starting DiscordDemoBot...");
         await discordClient.ConnectAsync();
     }
 
-    public override async Task StopAsync(CancellationToken cancellationToken)
+    public async Task StopAsync(CancellationToken cancellationToken)
     {
         discordClient.Logger.Log(LogLevel.Information, "Stopping DiscordDemoBot...");
         await discordClient.DisconnectAsync();
         discordClient.Dispose();
     }
-
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
-        => Task.CompletedTask;
 }
